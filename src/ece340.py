64ba732c906f79a1afe683c7ebb34b3ebef1123d
Nick <nickmooregot33@gmail.com>
2016-01-28 09:30:12 -0600

    modified some equations, deleted a backup .swp file

--- src/ece340.py
+++ src/ece340.py
@@ -5,13 +5,24 @@ from math import e
 from math import degrees
 from math import radians
 
+avogadro = 6.02e23
+atomic_weights = {
+		'Si':28.085,
+		'Ga':69.723,
+		'As':74.922,
+		'GaAs':144.645
+		}
 
+lattice_constants = { 		##in angstroms
+		'Si':5.43,
+		'GaAs':5.65
+		}
 
 def h(energy):
     if energy == 'joule':
-        return 6.62607004081*10E-34
+        return 6.62607004081e-34
     elif energy == 'eV':
-        return 4.13566766225*10E-15
+        return 4.13566766225E-15
 
 def E(energy,v): return h(energy) * v
 
